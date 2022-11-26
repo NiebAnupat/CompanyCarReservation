ab3096fd530d679cd0d510734b6ff1ce242e036f
Nieb Anupat <anupat.k64@rsu.ac.th>
2022-11-26 19:21:25 +0700

    Add Frontend

---
+++ store/User.js
@@ -0,0 +1,62 @@
+export const state = () => ({
+  user : {
+    EM_ID : null,
+    EM_ID_CARD : null,
+    EM_NAME : null,
+    EM_ADDRESS : null,
+    EM_GENDER : null,
+    EM_TEL : null,
+    EM_BIRTHDAY : null,
+    DP_ID : null,
+    P_ID : null,
+    EM_SALARY : null,
+    EM_HIRE_DATE : null,
+    EM_IS_ACTIVE : null,
+    EM_FINE : null,
+  },
+})
+
+export const getters = {
+  user : ( state ) => state.user,
+  fine : ( state ) => state.user.EM_FINE,
+}
+
+export const mutations = {
+  setUser( state, payload ) {
+    state.user = payload
+  }
+}
+
+export const actions = {
+  async getUser( { commit },emId ) {
+    const { data } = await this.$axios.$get(
+      '/employee',
+      {
+        body : { emId : emId }
+      } )
+    commit( 'setUser', data )
+  },
+
+  async authUser( { commit }, emId, cardId ) {
+    const { isAuth } = await this.$axios.$get(
+      '/employee/login',
+      {
+        body : {
+          emId : emId,
+          cardId : cardId
+        }
+      }
+    )
+
+    if ( isAuth ) {
+      const { data } = await this.$axios.$get(
+        '/employee',
+        {
+          body : { emId : emId }
+        } )
+      commit( 'setUser', data )
+    }
+
+
+  }
+}
